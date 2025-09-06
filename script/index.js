4bd19e342e38e25763639b0ac8897ae8e46fc1a1
nahid Islam Alif <nahidislamalif141@gmail.com>
2025-09-07 00:20:11 +0600

    display details using daisy modal

--- script/index.js
+++ script/index.js
@@ -23,6 +23,41 @@ const loadLevelWord = (id) => {
     });
 };
 
+const loradWordDetail= async (id) => {
+    const url =`https://openapi.programming-hero.com/api/word/${id}`;
+    const res = await fetch(url);
+    const details = await res.json();
+    displayWordDetails(details.data)
+
+}
+
+const displayWordDetails = (word) => {
+    console.log(word)
+    const detailsBox = document.getElementById("details-container")
+    detailsBox.innerHTML=`
+        <div>
+        <h2 class="font-bold text-2xl">${word.word} (<i class="fa-solid fa-microphone-lines"></i> ${word.pronunciation})</h2>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Meaning</h2>
+        <p>${word.meaning}</p>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Example</h2>
+        <p>${word.sentence}</p>
+      </div>
+      <div class="">
+        <span class="btn">Syn1</span>
+        <span class="btn">Syn1</span>
+        <span class="btn">Syn1</span>
+      </div>
+
+    `;
+    document.getElementById("word_modal").showModal()
+
+
+}
+
 // id: 103;
 // level: 2;
 // meaning: "খোলা";
@@ -56,7 +91,7 @@ const displayLevelWord = (words) => {
         <p class="font-semibold">Meaning / pronunciation</p>
         <div class="text-2xl font-bangla font-medium">${word.meaning ? word.meaning:  "অর্থ পাওয়া যায়নি" } / ${word.pronunciation}</div>
         <div class="flex justify-between items-center">
-          <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
+          <button onclick="loradWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
 
         </div>
